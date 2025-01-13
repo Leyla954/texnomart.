a2e8642e1b46a684da2cf5d130a5a33cf99cd48a
Leyla Muradzade <lvmuradzade55@gmail.com>
2025-01-13 13:42:22 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Scales from './assets/scales/Scales'
+import Detail from './assets/detail/Detail'
+
+const App = () => {
+  return (
+    <div>
+      <Scales/>
+      <Detail/>
+    </div>
+  )
+}
+
+export default App
